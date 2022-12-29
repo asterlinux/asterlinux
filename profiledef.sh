615f04868cb20b9503a21e4470d58e348d8579f5
sahanuser <sahan.user@gmail.com>
2022-12-29 11:17:39 +0530

    edit

---
+++ profiledef.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="asterlinux"
+iso_label="ASTER_$(date +%Y%m)"
+iso_publisher="Aster Linux <https://asterlinux.github.io>"
+iso_application="Aster Linux Live/Rescue CD"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
+           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/etc/shadow"]="0:0:0400"
+  ["/etc/gshadow"]="0:0:0400"
+#  ["/etc/skel/.config/qtile/autostart.sh"]="0:0:755"
+#  ["/etc/skel/.config/qtile/dmenu/dmenu-power"]="0:0:755"
+#  ["/etc/skel/.config/qtile/dmenu/dmenu-todo"]="0:0:755"
+#  ["/etc/skel/.bashrc"]="0:0:755"
+#  ["/etc/skel/.bash_profile"]="0:0:755"
+
+)
